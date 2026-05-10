957df18119739c3b0f73ae94ca87f84c47d0a8e7
sajidurrahman215 <sajidomarfaruk@gmail.com>
2026-05-10 14:02:03 +0600

    P05 - Print a mobile recharge receipt

---
+++ IdeaProjects/OOP_1/src/Problem5.java
@@ -0,0 +1,14 @@
+void main(){
+    System.out.println("================================");
+    System.out.println("\t\tROBI RECHARGE");
+    System.out.println("================================");
+    System.out.println("Number\t: 01XXXXXXXXX");
+    System.out.println("Amount\t: 100 BDT");
+    System.out.println("Bonus\t: 10 BDT");
+    System.out.println("Total\t: 110 BDT");
+    System.out.println("Date\t: 10 May 2026");
+    System.out.println("================================");
+    System.out.println("\t\tThank You!");
+    System.out.println("================================");
+
+}
