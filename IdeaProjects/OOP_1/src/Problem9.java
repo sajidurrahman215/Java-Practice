140df9f0fefc797b729f3d03583072eac7db0d47
sajidurrahman215 <sajidomarfaruk@gmail.com>
2026-05-10 14:42:36 +0600

    P09 - Print a Java logo text art

---
+++ IdeaProjects/OOP_1/src/Problem9.java
@@ -0,0 +1,9 @@
+void main(){
+    System.out.println("       oooo");
+    System.out.println("      o    o");
+    System.out.println("     o  JA  o");
+    System.out.println("    o   VA   o");
+    System.out.println("     o      o");
+    System.out.println("       oooo");
+    System.out.println("    JAVA RULES!");
+}
