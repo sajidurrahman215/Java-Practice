21e92f2a37d08503364966d726da2b7846012629
sajidurrahman215 <sajidomarfaruk@gmail.com>
2026-05-10 13:01:20 +0600

    P03 - Biodata Print

---
+++ IdeaProjects/OOP_1/src/Problem3.java
@@ -0,0 +1,11 @@
+void main(){
+    System.out.println("==========================");
+    System.out.println("\t\tMY BIODATA");
+    System.out.println("==========================");
+    System.out.println("Name\t: Sajidur Rahman");
+    System.out.println("Age \t: 25");
+    System.out.println("City\t: Dhaka");
+    System.out.println("Course\t: CSE");
+    System.out.println("Varsity\t: DIU");
+    System.out.println("==========================");
+}
