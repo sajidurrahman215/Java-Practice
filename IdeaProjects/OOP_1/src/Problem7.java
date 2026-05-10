be9b137d50507c824787dfe4c906c968ca241e09
sajidurrahman215 <sajidomarfaruk@gmail.com>
2026-05-10 14:24:10 +0600

    P07 - Print a number pyramid

---
+++ IdeaProjects/OOP_1/src/Problem7.java
@@ -0,0 +1,7 @@
+void main(){
+    System.out.println("        1");
+    System.out.println("      1 2 1");
+    System.out.println("    1 2 3 2 1");
+    System.out.println("  1 2 3 4 3 2 1");
+    System.out.println("1 2 3 4 5 4 3 2 1");
+}
