fe8cb0edd7446205c5eb80986e0c58f9d76d8d72
sajidurrahman215 <sajidomarfaruk@gmail.com>
2026-05-10 14:52:30 +0600

    P10 - Print a semester result card

---
+++ IdeaProjects/OOP_1/src/Problem10.java
@@ -0,0 +1,14 @@
+void main(){
+    System.out.println("╔══════════════════════════╗");
+    System.out.println("║     DIU RESULT CARD      ║");
+    System.out.println("║ Name   : Sajidur Rahman  ║");
+    System.out.println("║ ID     : XXXXXXXXX       ║");
+    System.out.println("║ Dept   : CSE             ║");
+    System.out.println("╠══════════════════════════╣");
+    System.out.println("║ OOP    :  A+             ║");
+    System.out.println("║ Math   :  A+             ║");
+    System.out.println("║ English:  A+             ║");
+    System.out.println("╠══════════════════════════╣");
+    System.out.println("║ CGPA   :  4.00           ║");
+    System.out.println("╚══════════════════════════╝");
+}
